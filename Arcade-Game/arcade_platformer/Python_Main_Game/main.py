434bb87bfb21b15d46ef1a7330ffe7f86dfc8336
Sebastian Klingshirn <s.klingshirn@reinhausen.com>
2023-01-11 08:39:00 +0100

    Draw Text as Object(faster)

--- Arcade-Game/arcade_platformer/Python_Main_Game/main.py
+++ Arcade-Game/arcade_platformer/Python_Main_Game/main.py
@@ -95,8 +95,7 @@ class MyGame(arcade.Window):
         #Should load Info Box?
         self.should_load_info_box = False
 
-
-        self.drawing_text = DrawText(self)
+        self.init_text()
 
         #setting the background-color for the map
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
@@ -200,7 +199,16 @@ class MyGame(arcade.Window):
         # Draw our Scene
         self.scene.draw()
 
-        DrawText(self.level)
+        self.profession_1.draw()
+        self.profession_2.draw()
+        self.profession_3.draw()
+        self.profession_4.draw()
+        self.profession_5.draw()
+        self.profession_6.draw()
+        self.profession_7.draw()
+        self.profession_8.draw()
+        self.profession_9.draw()
+
 
         # Activate the GUI camera before drawing GUI elements
         self.gui_camera.use()
@@ -364,6 +372,111 @@ class MyGame(arcade.Window):
         if self.player_sprite.center_x < stop_player_left_edge:
             self.player_sprite.center_x += 16
 
+    def init_text(self):
+        if self.level == 0:
+            self.drawWelcomeArea()
+
+        elif self.level == 1:
+            self.drawElectroIT()
+
+        else:
+                #Default
+                pass
+
+    def drawWelcomeArea(self):
+
+        # ID 1
+        self.profession_1 = arcade.Text(
+            "Fachkraft - Lagerlogistik",
+            1630,
+            430,
+            color= arcade.color.WHITE,
+            font_size= 26,
+            font_name=("Comic Sans MS")
+        )
+
+         
+        # ID 2
+        self.profession_2 = arcade.Text(
+            "Kaufmännisch",
+            start_x= 2940,
+            start_y= 880,
+            color= arcade.color.WHITE,
+            font_size= 26,
+            font_name=("Comic Sans MS")
+        )
+
+        # ID 3
+        self.profession_3 = arcade.Text(
+            "Fachinformatiker",
+            start_x = 1680,
+            start_y = 1400,
+            color= arcade.color.WHITE,
+            font_size= 22,
+            font_name = ("Comic Sans MS")
+        )
+
+        # ID 4
+        self.profession_4 = arcade.Text(
+            "Elektro - IT",
+            start_x = 350,
+            start_y = 1400,
+            color= arcade.color.WHITE,
+            font_size= 22,
+            font_name = ("Comic Sans MS")
+        )
+
+        # ID 5
+        self.profession_5 =arcade.Text(
+            "Elektro",
+            start_x = 2980,
+            start_y = 2300,
+            color= arcade.color.WHITE,
+            font_size= 22,
+            font_name = ("Comic Sans MS")
+        )
+
+        # ID 6
+        self.profession_6 = arcade.Text(
+            "Mechatronik",
+            start_x = 4560,
+            start_y = 2170,
+            color= arcade.color.WHITE,
+            font_size= 22,
+            font_name = ("Comic Sans MS")
+        )
+
+        # ID 7
+        self.profession_7 = arcade.Text(
+            "Industriemeachaniker",
+            start_x = 6000,
+            start_y = 2000,
+            color= arcade.color.WHITE,
+            font_size= 22,
+            font_name = ("Comic Sans MS")
+        )
+
+        # ID 8
+        self.profession_8 = arcade.Text(
+            "Werkzeugmechaniker",
+            start_x = 6500,
+            start_y = 2200,
+            color= arcade.color.WHITE,
+            font_size= 22,
+            font_name = ("Comic Sans MS")
+        )
+
+        # ID 9
+        self.profession_9 = arcade.Text(
+            "Zerspanner",
+            start_x = 7000,
+            start_y = 1200,
+            color= arcade.color.WHITE,
+            font_size= 22,
+            font_name = ("Comic Sans MS")
+        )
+
+         
 def main():
     """Main function"""
     window = MyGame()

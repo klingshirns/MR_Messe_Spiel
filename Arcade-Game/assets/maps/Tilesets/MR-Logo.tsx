738ef86fca07144a099944c78581c9eb8d2d35a3
HRT Daniel Steinberger <d.steinberger@reinhausen.com>
2023-01-12 11:03:58 +0100

    DS/enemies and lifes added and MR-coins improved

--- Arcade-Game/assets/maps/Tilesets/MR-Logo.tsx
+++ Arcade-Game/assets/maps/Tilesets/MR-Logo.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="MR-Logo" tilewidth="128" tileheight="127" tilecount="1" columns="0">
+<tileset version="1.9" tiledversion="1.9.0" name="mr-logo" tilewidth="128" tileheight="127" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="5">
+ <tile id="6">
   <image width="128" height="127" source="../../images/items/mr-logo.png"/>
  </tile>
 </tileset>

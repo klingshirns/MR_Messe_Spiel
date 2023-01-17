d92aae3b773e145949fdd39c7bad4864f3a01d85
Sebastian Klingshirn <s.klingshirn@reinhausen.com>
2023-01-17 10:33:41 +0100

    KS/ comments in main.py and creating .exe

---
+++ Arcade-Game/arcade_platformer/Startmenü/Startmenü/WrongPassword.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Startmenü
+{
+    /// <summary>
+    /// Interaktionslogik für WrongPassword.xaml
+    /// </summary>
+    public partial class WrongPassword : Window
+    {
+        public WrongPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void Closebut_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
